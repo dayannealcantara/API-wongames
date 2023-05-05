6bfdb026cb820ccdd4d31e79140d0d2570469c5e
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-05 16:32:44 -0300

    feat:creating data with service create

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,40 +1,58 @@
 "use strict";
 const axios = require("axios");
+const slugify = require("slugify");
 
 async function getGameInfo(slug) {
-  const jsdom = require("jsdom");
-  const { JSDOM } = jsdom;
-  const body = await axios.get(`https://www.gog.com/game/${slug}`);
-  const dom = new JSDOM(body.data);
+  try {
+    const jsdom = require("jsdom");
+    const { JSDOM } = jsdom;
+    const body = await axios.get(`https://www.gog.com/game/${slug}`);
+    const dom = new JSDOM(body.data);
 
-  const ratingElement = dom.window.document.querySelector(
-    ".age-restrictions__icon use"
-  );
+    const description = dom.window.document.querySelector(".description");
 
-  const description = dom.window.document.querySelector(".description");
-
-  return {
-    rating: ratingElement
-      ? ratingElement
-          .getAttribute("xlink:href")
-          .replace(/_/g, "")
-          .replace(/[^\w-]+/g, "")
-      : "Free",
-    short_description: description.textContent.trim().slice(0, 160),
-    description: description.innerHTML,
-  };
+    return {
+      rating: "BR0",
+      short_description: description.textContent.trim().slice(0, 160),
+      description: description.innerHTML,
+    };
+  } catch (e) {
+    console.log("getGameInfo", Exception(e));
+  }
 }
+
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { strict: true, lower: true }),
+    });
+  }
+}
+
 
 module.exports = {
   populate: async (params) => {
-    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+    try {
+      const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&${qs.stringify(
+        params
+      )}`;
 
-    const {
-      data: { products },
-    } = await axios.get(gogApiUrl);
+      const {
+        data: { products },
+      } = await axios.get(gogApiUrl);
 
-    // console.log(products[0]);
-
-    console.log(await getGameInfo(products[1].slug));
+      await createManyToManyData(products);
+      await createGames(products);
+    } catch (e) {
+      console.log("populate", Exception(e));
+    }
   },
 };

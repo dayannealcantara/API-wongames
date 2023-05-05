568fb7401ed844f289123d61635b2f3c07e6c6c8
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-05 15:23:48 -0300

    feat: creating data scrapper for popular API

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,7 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    console.log("Starting to populate...")
+    await strapi.services.game.populate()
+    ctx.send("Finished populating!")
+  }
+};
